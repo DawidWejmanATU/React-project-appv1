2c083a78aba81eba423d73970851dd7ff8a60176
DawidWejmanATU <G00388437@atu.ie>
2023-12-20 17:56:18 +0000

    reading.js file to read data

---
+++ src/components/reading.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Films from "./films";
+
+
+function Reading(){
+    const {data, setData} = useState([]);
+
+    useEffect(
+        ()=>{
+            axios.get('http://localhost:4000/api/films')
+            .then(
+                (Response)=>{
+                    setData(response.data)
+                }
+            )
+            .catch(
+                (error)=>{
+                    console.log(error);
+                }
+            )
+        }, []
+    );
+
+        const ReloadData = (e)=>{
+            axios.get('http://localhost:4000/api/films')
+            .then(
+                (response)=>{
+                    setData(response.data)
+                }
+            )
+            .catch(
+                (error)=>{
+                    console.log(error);
+                }
+            )
+        }
+        return(
+            <div>
+                <h2>Reading</h2>
+                <Films myfilms={data} Reload={ReloadData}></Films>
+            </div>
+        );
+
+}
+
+export default Reading;
